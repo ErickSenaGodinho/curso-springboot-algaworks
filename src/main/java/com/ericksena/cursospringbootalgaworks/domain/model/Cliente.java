04a98aed325dda0e711707fe89b763fd92d0c61f
Erick Sena <ericksena621@gmail.com>
2022-10-26 13:27:54 -0300

    Update: Validation with Bean Validation

--- src/main/java/com/ericksena/cursospringbootalgaworks/domain/model/Cliente.java
+++ src/main/java/com/ericksena/cursospringbootalgaworks/domain/model/Cliente.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,12 +24,18 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     @Column(nullable = false, length = 60)
     private String nome;
 
+    @NotBlank
+    @Email
     @Column(nullable = false)
     private String email;
 
+    @NotBlank
+    @Size(max = 15)
     @Column(nullable = false, length = 15)
     private String telefone;
 }
